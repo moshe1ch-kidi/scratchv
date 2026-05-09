4fce922ba1ba6a6cf3bc38f99761d6c251d284f4
moshe1ch-kidi <moshe1.ch@gmail.com>
2026-05-09 16:42:16 +0300

    Add files via upload

--- components/PaintEditor.tsx
+++ components/PaintEditor.tsx
@@ -1,4 +1,4 @@
- import React, { useState, useRef, useEffect, useLayoutEffect } from 'react';
+import React, { useState, useRef, useEffect, useLayoutEffect } from 'react';
 import { Sprite } from '../types';
 
 // --- Type Definitions ---
